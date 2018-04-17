15b1337b9c8a633131d8d97f904582c4fd74393b
Molivcosta <38051913+Molivcosta@users.noreply.github.com>
2018-04-17 20:13:13 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,36 @@
+/* Aluno: Mateus Oliveira Costa
+   Matricula: 11711EAU018 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int estado = 0, i = 0;
+	char bits[256];
+	printf("Digite a sequencia de bits: ");
+	scanf("%s", &bits);
+	while(bits[i] != '\0')
+	{
+		if(bits[i] == '0' && estado == 0 || bits[i] == '1' && estado == 1)
+		{
+			estado = 0;
+		}
+		else if(bits[i] == '1' && estado == 0 ||  bits[i] == '0' && estado == 2)
+		{
+			estado = 1;
+		}
+		else if(bits[i] == '0' && estado == 1 ||  bits[i] == '1' && estado == 2)
+		{
+			estado = 2;
+		}
+		++i;
+	}
+	if(estado == 0)
+	{
+		printf("\n%s e multiplo de 3",bits);
+	}
+	else{
+		printf("\n%s nao e",bits);
+	}
+	return 0;
+}
